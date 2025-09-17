28b23127ab3d46903fc330756b16b8ee61c77d6e
zombox0633 <ghostzb@outlook.com>
2025-09-18 00:27:47 +0700

    add: HashPassword and JwtService to ApplicationExtensions

--- src/extensions/applicationExtensions.cs
+++ src/extensions/applicationExtensions.cs
@@ -1,16 +1,24 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 using todolist.iservice;
 using todolist.service;
 using user.data;
 using user.iservice;
 using user.service;
+using utils.ihashPassword;
+using utils.hashPassword;
+using utils.ijwt;
+using utils.jwt;
 
 namespace extensions;
 
 public static class ApplicationExtensions
 {
-  // Add all application services
+  //------------------------ Add all application services -------------------------------
   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
   {
     //TodoList
@@ -18,10 +26,39 @@ public static class ApplicationExtensions
 
     // User Services
     services.AddScoped<IUserService, UserService>();
+    services.AddScoped<IHashPassword, HashPassword>();
+    services.AddScoped<IJwtService, JwtService>();
     return services;
   }
 
-  // Configure database connection
+  //------------------------ Configure JWT Authentication ---------------------------------
+  public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+          Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]!)
+        ),
+        ValidateIssuer = true,
+        ValidIssuer = "facenotebook_app",
+        ValidateAudience = true,
+        ValidAudience = "earth_ten",
+        ValidateLifetime = true, // เช็ค token หมดอายุ
+        ClockSkew = TimeSpan.Zero // ไม่เผื่อเวลา
+      };
+    });
+
+    services.AddAuthorization();
+
+    return services;
+  }
+
+
+  //------------------------ Configure database connection --------------------------------
   public static IServiceCollection AddDatabaseServices(this IServiceCollection services, string connectionString)
   {
     services.AddDbContext<ApplicationDbContext>(options =>
@@ -30,7 +67,7 @@ public static class ApplicationExtensions
     return services;
   }
 
-  // Configure CORS policy
+  //------------------------ Configure CORS policy --------------------------------------
   public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
   {
     services.AddCors(options =>
@@ -42,7 +79,7 @@ public static class ApplicationExtensions
           .AllowAnyHeader();
       });
     });
-        
+
     return services;
   }
 }
